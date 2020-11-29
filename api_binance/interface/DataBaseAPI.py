923167a8579b0f949d4edf8ca8587f144a20bf11
sofiat-capital <dev.sofiat@gmail.com>
2020-11-29 17:20:14 -0600

    complete SQLEngine

--- api_binance/interface/DataBaseAPI.py
+++ api_binance/interface/DataBaseAPI.py
@@ -2,10 +2,6 @@ import os
 import numpy as np
 from .base_api import BaseAPI
 
-import mysql.connector
-from mysql.connector import Error
-from mysql.connector import errorcode
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -18,30 +14,37 @@ from sqlalchemy.schema import SchemaItem
 from sqlalchemy.types import TypeEngine
 from sqlalchemy import (create_engine, MetaData, Column,
         Integer, String, Table)
+from sqlalchemy.orm import sessionmaker
 import pprint
 
 import os, sys
 
 
+# CREATES SQL ENGINE (CONNECTION) TO DATABASE WITH SQLALCHEMY
 class SQLEngine(BaseAPI):
     def __init__(self):
         self.keychain = {'mysql' : os.environ.get('mysql_key')}
 
         try:
             url = 'mysql+pymysql://root:{}@localhost/sofiat'.format(self.keychain.get('mysql'))
-            self.engine = create_engine(url)
+            self.Engine = create_engine(url)
+            self.Engine.connect()
+
+            self.Session = sessionmaker()
+            self.Session.configure(bind=self.Engine)
 
         except:
             self.log('Error connecting to database')
 
         self.base = automap_base()
-        self.base.prepare(self.engine, reflect=True)
+        self.base.prepare(self.Engine, reflect=True)
         self.models = self.base.classes
 
         self.log('initializing SQL Engine')
         return
 
 
+#
 class DataBaseAPI(BaseAPI):
     def __init__(self):
         BaseAPI.__init__(self)
@@ -56,56 +59,65 @@ class DataBaseAPI(BaseAPI):
                                       auth_plugin='mysql_native_password')
 
         self.engine = SQLEngine()
+
+
+        ### Models
+        self.Category    = self.engine.models.get('category')
+        self.Product     = self.engine.models.get('product')
+        self.DayCandle   = self.engine.models.get('dayCandle')
+        self.Transaction = self.engine.models.get('transaction')
+
+
         return
 
     ############################################################################
     ### Insert FUNCTIONS
     ############################################################################
-
-
     def InsertDayCandle(self, daycandles):
         '''Precondition: daycandles is the result of self.kline
         '''
-        columns = ['close_time', 'low', 'high', 'open', 'close', 'volume', 'number_of_trades']
-
-        for column in columns[1:-1]:
-            daycandles[column] = np.array(daycandles[column], dtype=float)
-
-        daycandles[columns[-1]] = np.array(daycandles[column], dtype=int)
-
-
-        mySql_insert_query = """INSERT INTO dayCandle (iddayCandle, fk_idproduct_dayCandle, date, low, hi, open, close, volume, numtrades)
-                               VALUES
-                               (null, %s, %s, %s, %s, %s, %s, %s, %s) """
-
-        cursor = self.cnx.cursor()
+        session = self.engine.Session()
 
         for i, candle in daycandles.iterrows():
-            symbol_id = self._get_ticker_id(candle['symbol'])
-            recordTuple = list(candle[columns])
-            recordTuple.insert(0, str(symbol_id))
-            cursor.execute(mySql_insert_query, recordTuple)
-            self.cnx.commit()
 
-        self.log('Record inserted successfully into dayCandle {}'.format(cursor.rowcount))
-        cursor.close()
+            element = self.DayCandle(fk_idproduct_dayCandle = self._get_ticker_id(candle['symbol']),
+                                     date   = candle['close_time'],
+                                     low    = float(candle['low']),
+                                     hi     = float(candle['high']),
+                                     open   = float(candle['open']),
+                                     close  = float(candle['close']),
+                                     volume = float(candle['volume']),
+                                     numtrades = int(candle['number_of_trades'])
+                                   )
+
+            session.add(element)
+
+        self.log('Record inserted successfully into dayCandle {}'.format(candle['symbol']))
+
+        session.commit()
+        session.close()
         return
 
     # ENTERS DATA INTO TRANSACTION TO TRACK TRADE HISTORY
-    def InsertTransaction(self, input_ticker, value=0):
-        product_id = self._get_ticker_id(input_ticker)
-
-        mySql_insert_query = """INSERT INTO transaction (idtransaction, fk_idproduct_transaction, transactionTime, buySell, price) VALUES (null, %s, %s, true, %s)"""
+    def InsertTransaction(self, input_ticker, buy_sell, price, quantity):
+        session = self.engine.Session()
         timestamp = self.current_time
+        product_id = self._get_ticker_id(input_ticker)
+        if not product_id:
+            self.log('No Product for {}'.format(input_ticker))
+            return None
 
-        recordTuple = (str(product_id), timestamp, str(value))
-        self.log(recordTuple)
-        cursor = self.cnx.cursor()
-        self.log('Executing....')
-        cursor.execute(mySql_insert_query, recordTuple)
-        self.cnx.commit()
-        cursor.close()
+        transaction = self.Transaction(fk_idproduct_transaction = product_id,
+                         transactionTime          = timestamp,
+                         buySell                  = buy_sell,
+                         price                    = price,
+                         quantity                 = quantity)
+
+        session.add(transaction)
+        session.commit()
+
         self.log('Success')
+        session.close()
         return
 
 
@@ -113,30 +125,54 @@ class DataBaseAPI(BaseAPI):
     ### HIDDEN FUNCTIONS
     ############################################################################
     # OBTAINS VALUE STORED IN DATABASE ASSOCIATED WITH TICKER PASSED
+
+    def CreateCategory(self, name):
+        session = self.engine.Session()
+        category = session.query(self.Category).filter(self.Category.name == name).first()
+
+        if not category:  ### doesn't exist
+            category = self.Category(name = name)
+            session.add(category)
+            session.commit()
+            self.log('Committed new Category field:  {}'.format(category))
+
+        else:
+            self.log('Category {} exists'.format(name))
+
+        return category
+
+        session.close()
+
+
+    def CreateProduct(self, productName, categoryName):
+        session = self.engine.Session()
+        product = session.query(self.Product).filter(self.Product.ticker == productName).first()
+
+        if not product:  ## if product == None (doesn't exists)
+            category = self.CreateCategory(categoryName)
+            product = self.Product(ticker = productName,
+                                   fk_idcategory_product = category.idcategory)
+
+            session.add(product)
+            session.commit()
+            self.log('Committed new Product field:  {}'.format(product))
+
+        else:
+            self.log('Product {} exists'.format(product))
+
+        session.close()
+        return product
+
+
+
     def _get_ticker_id(self, ticker):
-        # SELECT gets the associated product NUMBER from the ticker passed
-        select_stmt = "SELECT idproduct FROM product WHERE ticker = '{}'".format(ticker)
+        session = self.engine.Session()
 
-        cursor = self.cnx.cursor()
-        cursor.execute(select_stmt)
-        result = cursor.fetchall()
-        result = np.array(result).flatten()[0]   # List becomes single value
-        cursor.close()
-        return result
+        product = session.query(self.Product).filter(self.Product.ticker == ticker).first()
 
+        if not product:
+            self.log("Product {} doesn't exist".format(ticker))
+            return None
 
-    '''
-    def InsertDayCandle_(self, daycandles):
-
-        DayCandle =
-
-        MODEL = self.engine.models.get('dayCandle')(date      = daycandles['close_time'],
-                                            low       = daycandles['low'],
-                                            hi        = daycandles['high'],
-                                            open      = daycandles['open'],
-                                            close     = daycandles['close'],
-                                            volume    = daycandles['volume'],
-                                            numtrades = daycandles['number_of_trades']
-                                          )
-        return
-    '''
+        session.close()
+        return product.idproduct
