7effca631dbddf6e68590f1effe4eb10c793027e
sofiat-capital <dev.sofiat@gmail.com>
2020-12-07 17:18:12 -0600

    reform tables, add BinanceOrder/Fills

--- api_binance/MasterAPI.py
+++ api_binance/MasterAPI.py
@@ -55,3 +55,75 @@ class MasterAPI(BaseAPI):
 if __name__ == '__main__':
     api = MasterAPI()  #Creates object (instance of BinanceAPI class)
 #    api.run()
+
+    TEST_ORDER = {
+    "symbol"                   : "BTCUSDT",
+    "idbinanceOrder"           :  1,
+    "fk_idproduct_binanceOrder":  1,
+    "orderListId"               : -1,
+    "clientOrderId"            : 'TEST',
+    "transactTime"             : api.current_time,
+    "price"                    : 10.0,
+    "origQty"                  : 9.99,
+    "executedQty"              : 9.99,
+    "cummulativeQuoteQty"      : 9.99,
+    "status"                   : "OPEN",
+    "timeInForce"              : "TEST TIME",
+    "type"                     : "MARKET",
+    "side"                     : "BUY"
+    }
+
+    ORDER_RESPONSE = {
+      "symbol": "BTCUSDT",
+      "orderId": 1,
+      "orderListId": -1,
+      "clientOrderId": "6gCrw2kRUAF9CvJDGP16IP",
+      "transactTime": 1507725176595,
+      "price": "0.00000000",
+      "origQty": "10.00000000",
+      "executedQty": "10.00000000",
+      "cummulativeQuoteQty": "10.00000000",
+      "status": "FILLED",
+      "timeInForce": "GTC",
+      "type": "MARKET",
+      "side": "SELL",
+      "fills": [
+        {
+          "price": "4000.00000000",
+          "qty": "1.00000000",
+          "commission": "4.00000000",
+          "commissionAsset": "USDT"
+        },
+        {
+          "price": "3999.00000000",
+          "qty": "5.00000000",
+          "commission": "19.99500000",
+          "commissionAsset": "USDT"
+        },
+        {
+          "price": "3998.00000000",
+          "qty": "2.00000000",
+          "commission": "7.99600000",
+          "commissionAsset": "USDT"
+        },
+        {
+          "price": "3997.00000000",
+          "qty": "1.00000000",
+          "commission": "3.99700000",
+          "commissionAsset": "USDT"
+        },
+        {
+          "price": "3995.00000000",
+          "qty": "1.00000000",
+          "commission": "3.99500000",
+          "commissionAsset": "USDT"
+        }
+      ]
+    }
+
+    ORDER_RESPONSE['idbinanceOrder'] = 1
+
+
+
+    api.DataBaseAPI.InsertBinanceOrder(TEST_ORDER)
+    api.DataBaseAPI.InsertBinanceFills(ORDER_RESPONSE)
