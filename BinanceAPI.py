17b366f4ddf1eeeac29761b7d7abf6806adaf5da
sofiat-capital <dev.sofiat@gmail.com>
2021-02-03 18:05:19 -0600

    ETH transition

--- BinanceAPI.py
+++ BinanceAPI.py
@@ -179,6 +179,7 @@ class BinanceAPI(BaseAPI):
         params.update(self.gen_authenticator(params))
         headers = self.gen_headers()
         body = self.gen_payload(params)
+        print(params)
         session.headers.update(headers)
         # ? symbol separates the endpoint (page) from encoded information
         url += '?' + body
@@ -186,8 +187,8 @@ class BinanceAPI(BaseAPI):
         self.log("Sending Order Request {}")#.format(json.dumps(params, indent=4)))
         #self.response = requests.post(url, headers).content
         self.response = json.loads(session.post(url).content)
-        #self.response = test_payloads.ORDER_RESPONSE
-        self.log(json.dumps(self.response, indent = 4))
+        self.response = test_payloads.ORDER_RESPONSE
+        #self.log(json.dumps(self.response, indent = 4))
         session.close()
         return self.response
 
@@ -206,12 +207,13 @@ class BinanceAPI(BaseAPI):
         headers = self.gen_headers()
         body = self.gen_payload(params)
         session.headers.update(headers)
+        #print(params)
         # ? symbol separates the endpoint (page) from encoded information
         url += '?' + body
-        self.log(url)
+        #self.log(url)
         self.log("Sending Order Request") #.format(json.dumps(params, indent=4)))
-        #self.response = requests.post(url, headers).content
-        self.response = json.loads(session.post(url).content)
+        self.response = requests.post(url, headers).content
+        #self.response = json.loads(session.post(url).content)
         self.log(json.dumps(self.response, indent = 4))
         session.close()
         return self.response
